3fd61f750837c6daf830a6b5cac5f7b36fecfc5f
Trịnh Hải <trinhhai758@gmail.com>
2025-10-24 20:30:49 +0700

    Rename file

--- app/src/main/java/com/example/truyengo/RegisterActivity.java
+++ app/src/main/java/com/example/truyengo/RegisterActivity.java
@@ -8,6 +8,6 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup_page);
+        setContentView(R.layout.register_page);
     }
 }
